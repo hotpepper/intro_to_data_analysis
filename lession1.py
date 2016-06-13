c4cf2f5d2d6fdf36323a240e6fdcc35e0f2fc81e
SHostetter <aasere@gmail.com>
2016-06-13 10:30:43 -0400

    inital work

---
+++ lession1.py
@@ -0,0 +1,65 @@
+import csv
+from datetime import datetime as dt
+
+# Takes a date as a string, and returns a Python datetime object. 
+# If there is no date given, returns None
+def parse_date(date):
+    if date == '':
+        return None
+    else:
+        return dt.strptime(date, '%Y-%m-%d')
+    
+# Takes a string which is either an empty string or represents an integer,
+# and returns an int or None.
+def parse_maybe_int(i):
+    if i == '':
+        return None
+    else:
+        return int(i)
+
+    
+
+enrollments_filename = 'enrollments.csv'
+engagement_filename = 'daily_engagement.csv'
+submissions_filename = 'project_submissions.csv'
+
+def read_csv(filename):
+    with open(filename, 'rb') as f:
+        reader = csv.DictReader(f)
+        return list(reader)
+
+enrollments = read_csv(enrollments_filename)
+daily_engagement = read_csv(engagement_filename)
+project_submissions = read_csv(submissions_filename)
+
+print engagement_filename[0]
+print daily_engagement[0]
+print project_submissions[0]
+
+# Clean up the data types in the enrollments table
+for enrollment in enrollments:
+    enrollment['cancel_date'] = parse_date(enrollment['cancel_date'])
+    enrollment['days_to_cancel'] = parse_maybe_int(enrollment['days_to_cancel'])
+    enrollment['is_canceled'] = enrollment['is_canceled'] == 'True'
+    enrollment['is_udacity'] = enrollment['is_udacity'] == 'True'
+    enrollment['join_date'] = parse_date(enrollment['join_date'])
+    
+print enrollments[0]
+
+
+# Clean up the data types in the engagement table
+for engagement_record in daily_engagement:
+    engagement_record['lessons_completed'] = int(float(engagement_record['lessons_completed']))
+    engagement_record['num_courses_visited'] = int(float(engagement_record['num_courses_visited']))
+    engagement_record['projects_completed'] = int(float(engagement_record['projects_completed']))
+    engagement_record['total_minutes_visited'] = float(engagement_record['total_minutes_visited'])
+    engagement_record['utc_date'] = parse_date(engagement_record['utc_date'])
+    
+print daily_engagement[0]
+
+# Clean up the data types in the submissions table
+for submission in project_submissions:
+    submission['completion_date'] = parse_date(submission['completion_date'])
+    submission['creation_date'] = parse_date(submission['creation_date'])
+
+project_submissions[0]
